0dcd64433ad38546813a14815231eab0a98a655c
ronit0010 <ronitpathak12345@gmail.com>
2024-12-24 01:24:10 +0530

    Updated app with new changes

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://ggyitpwqevepkqxqutkn.supabase.co";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdneWl0cHdxZXZlcGtxeHF1dGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MTY0MzgsImV4cCI6MjA1MDI5MjQzOH0.zjqT8gPCSpCQayXTqw3QM4S-HkfYPwW7hgodCafNM8Q";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
