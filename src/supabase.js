fe05f32bf961004e626772b064e0100f4ae5ddca
ronit0010 <ronitpathak12345@gmail.com>
2024-12-24 00:33:35 +0530

    Secure Supabase API keys using environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://ggyitpwqevepkqxqutkn.supabase.co"; // Replace with your Supabase project URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdneWl0cHdxZXZlcGtxeHF1dGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MTY0MzgsImV4cCI6MjA1MDI5MjQzOH0.zjqT8gPCSpCQayXTqw3QM4S-HkfYPwW7hgodCafNM8Q"; // Replace with your Supabase anon key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
